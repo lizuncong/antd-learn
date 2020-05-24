16bac2df90b606eeac1cddc2c86785ca9ddcba58
lizuncong <2900170896@qq.com>
2020-05-24 23:49:03 +0800

    首页

--- _theme/template/Home.jsx
+++ _theme/template/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router';
 require('../static/style');
 
 export default function Home() {
 
   return (
     <div className="home">
-      首页
+      <div className="home-content">
+        <h3>react ui</h3>
+        <div className="home-link">
+          <a
+              href="https://github.com/lizuncong/antd-learn"
+              className="github"
+              target="_blank"
+          >
+            GitHub
+          </a>
+          <Link className="start" to="/articles">
+            Get Started
+          </Link>
+        </div>
+        <div className="slogan">
+          React组件库，仅供学习
+        </div>
+      </div>
     </div>
   )
 }
